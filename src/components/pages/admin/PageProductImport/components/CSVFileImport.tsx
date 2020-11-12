b7614e0d078c2b77ae8aad96fa5d84e45688f43d
Aliaksandr Hordzin <Aliaksandr_Hordzin@epam.com>
2020-11-12 23:15:03 +0300

    Task-5 - import url is added

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -38,14 +38,15 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         params: {
           name: encodeURIComponent(file.name)
         }
-      })
-      console.log('File to upload: ', file.name)
-      console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
+      });
+      const { signedUrl } = response.data;
+      console.log('File to upload: ', file.name);
+      console.log('Uploading to: ', signedUrl);
+      const result = await fetch(signedUrl, {
         method: 'PUT',
         body: file
-      })
-      console.log('Result: ', result)
+      });
+      console.log('Result: ', result);
       setFile('');
     }
   ;
